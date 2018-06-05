939157949c443dcd968a73a3a335f66946fab0b9
Hamida Hai <39965120+hai-hamida@users.noreply.github.com>
2018-06-05 14:59:52 -0400

    Add files via upload

---
+++ 4.2Animals.py
@@ -0,0 +1,5 @@
+animals = [ 'cheetah' , 'jaguar' , 'panther' ]
+for similarities in animals:
+	print('A ' + similarities + ' is a fast animal. ')
+print( 'All of the above animals are known for their speed.' )
+	
