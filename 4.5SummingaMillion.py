deabc8b7c6ff126688fbb7e609b346e450db0971
Hamida Hai <39965120+hai-hamida@users.noreply.github.com>
2018-06-12 13:29:47 -0400

    Add files via upload

---
+++ 4.5SummingaMillion.py
@@ -0,0 +1,11 @@
+#This prints out the minimum, maximum, and the sum of a list from 1 to 1000000
+#The list is created by wrapping the range() function in a list() 
+list1 = list(range(1,1000001))
+print(min(list1))
+print(max(list1))
+print(sum(list1))
+	
+
+
+
+	
